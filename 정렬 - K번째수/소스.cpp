054aec96fa95d5454c487741868d835a94388c7f
box8741 <box8741@gmail.com>
2021-10-26 19:01:14 +0900

    add : K번째수 문제 완료

--- 정렬 - K번째수/소스.cpp
+++ 정렬 - K번째수/소스.cpp
@@ -15,9 +15,32 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
+
+    for (int i = 0; i < commands.size(); i++) {
+        vector<int> v;
+        int start = commands[i][0];
+        int end = commands[i][1];
+        int position = commands[i][2] - 1;
+
+        for (int j = start - 1; j < end; j++) {
+            v.push_back(array[j]);
+        }
+
+        sort(v.begin(), v.end());
+
+        answer.push_back(v[position]);
+    }
+
     return answer;
 }
 
 void main() {
+    vector<int> array = { 1, 5, 2, 6, 3, 7, 4 };
+    vector<vector<int>> commands = { {2, 5, 3},{4, 4, 1},{1, 7, 3} };
+
+    vector<int> answer = solution(array, commands);
     
+    for (int i=0; i < answer.size(); i++) {
+        cout << answer[i] << endl;
+    }
 }
