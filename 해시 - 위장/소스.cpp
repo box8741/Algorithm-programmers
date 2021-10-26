041d1fd0da402e4b49169909951dbe91010f0277
box8741 <box8741@gmail.com>
2021-10-26 18:12:25 +0900

    add : 위장 문제 완료

--- 해시 - 위장/소스.cpp
+++ 해시 - 위장/소스.cpp
@@ -10,14 +10,26 @@
 #include <algorithm>
 #include <queue>
 #include <math.h>
+#include <unordered_map>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 0;
+    int answer = 1;
+
+    unordered_map <string, int> attributes;
+    for (int i = 0; i < clothes.size(); i++)
+        attributes[clothes[i][1]]++;
+    for (auto it = attributes.begin(); it != attributes.end(); it++)
+        answer *= (it->second + 1);
+
+    answer--;
+
     return answer;
 }
 
-
 void main() {
-    
+    vector<vector<string>> clothes = { {"yellowhat", "headgear"},{"bluesunglasses", "eyewear"},{"green_turban", "headgear"}};
+    //vector<vector<string>> clothes = { {"crowmask", "face"} ,{"bluesunglasses", "face"},{"smoky_makeup", "face"} };
+    int answer = solution(clothes);
+    cout << "위장 개수 : " << answer;
 }
