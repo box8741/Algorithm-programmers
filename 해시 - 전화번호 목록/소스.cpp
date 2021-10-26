4395513f3945aea44cceb4e1377a535550a85743
box8741 <box8741@gmail.com>
2021-10-26 12:50:41 +0900

    add : 전화번호 목록 완료

--- 해시 - 전화번호 목록/소스.cpp
+++ 해시 - 전화번호 목록/소스.cpp
@@ -12,7 +12,20 @@
 #include <math.h>
 using namespace std;
 
-int main() {
-	cout << "�׽�Ʈ";
-	return 0;
+bool solution(vector<string> v) {
+    sort(v.begin(), v.end());
+    for (int i = 1; i < v.size(); i++) {
+        int len = v[i - 1].length();
+        if (v[i - 1] == v[i].substr(0, len)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void main() {
+    vector<string> phone_book = { "123","456","789" };
+    bool answer = solution(phone_book);
+    cout << "전화번호 결과 : " << answer;
 }
