79aa1c77c5ace29897d504b278b3019d42f5263b
box8741 <box8741@gmail.com>
2021-10-27 14:02:47 +0900

    add : 가장 큰 수 문제 해결

--- 정렬 - 가장 큰 수/소스.cpp
+++ 정렬 - 가장 큰 수/소스.cpp
@@ -13,11 +13,29 @@
 #include <unordered_map>
 using namespace std;
 
+bool compare(string a, string b) {
+    return a + b > b + a;
+}
+
 string solution(vector<int> numbers) {
     string answer = "";
+    vector<string> v;
+    for (int i = 0; i < numbers.size(); i++)
+        v.push_back(to_string(numbers[i]));
+    
+    sort(v.begin(), v.end(), compare);
+
+    for (int i = 0; i < v.size(); i++) 
+        answer += v[i];
+    
+    if (answer.at(0) == '0') answer = "0";
     return answer;
 }
 
 void main() {
+    //vector<int> numbers = { 0,0,0 };
+    vector<int> numbers = { 3, 30, 34, 5, 9 };
 
+    string answer = solution(numbers);
+    cout << "가장 큰 수 : " << answer << endl;
 }
