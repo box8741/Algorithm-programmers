d6451b8663c129f6b5d7f4159c14cefc38bfc59f
box8741 <box8741@gmail.com>
2021-10-27 20:08:05 +0900

    add : 프린터 문제 추가

---
+++ 스택,큐 - 프린터/소스.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <math.h>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    return answer;
+}
+
+void main() {
+
+}
