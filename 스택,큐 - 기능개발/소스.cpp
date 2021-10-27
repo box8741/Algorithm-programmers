07809d5777cc901cb218d7bd7b7bf1081e35e274
box8741 <box8741@gmail.com>
2021-10-27 19:58:06 +0900

    add : 기능개발 문제 완료

--- 스택,큐 - 기능개발/소스.cpp
+++ 스택,큐 - 기능개발/소스.cpp
@@ -11,12 +11,40 @@
 #include <queue>
 #include <math.h>
 #include <unordered_map>
+#include <stack>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
+    queue<int> s;
+    int sum = 0;
+
+    for (int i = 0; i < progresses.size(); i++) {
+        int day;
+        if ((100 - progresses[i]) % speeds[i] != 0) day = ((100 - progresses[i]) / speeds[i])+1;
+        else day = (100 - progresses[i]) / speeds[i];
+        
+        if (s.empty()) s.push(day);
+
+        if (s.back() >= day) sum++;
+        else {
+            s.push(day);
+            answer.push_back(sum);
+            sum = 1;
+        }
+    }
+
+    if (sum != 0) answer.push_back(sum);
+
     return answer;
 }
 
 void main() {
+    vector<int> progresses = { 95, 90, 99, 99, 80, 99 };
+    vector<int> speeds = { 1, 1, 1, 1, 1, 1 };
+
+    vector<int> answer = solution(progresses, speeds);
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << endl;
+    }
 }
