6595a7d3494a7c896403804f059e89bb78c81ec6
box8741 <box8741@gmail.com>
2021-10-27 16:30:16 +0900

    add : H-Index 문제 해결

--- 정렬 - H-Index/소스.cpp
+++ 정렬 - H-Index/소스.cpp
@@ -15,8 +15,20 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
+    sort(citations.begin(), citations.end(), greater<>());
+
+    for (int i = 0; i < citations.size(); i++) {
+        if (citations[i] >= i+1) {
+            answer += 1;
+        }
+    }
+
     return answer;
 }
 
 void main() {
+    vector<int> numbers = { 3, 0, 6, 1, 5 };
+
+    int answer = solution(numbers);
+    cout << "H-Index : " << answer << endl;
 }
