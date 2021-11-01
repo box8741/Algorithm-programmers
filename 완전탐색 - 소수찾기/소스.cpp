9e6d4774deee55962da6044ce4cde2d1da7490df
box8741 <box8741@gmail.com>
2021-11-01 17:49:20 +0900

    add : 소수찾기 문제 추

---
+++ 완전탐색 - 소수찾기/소스.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <math.h>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+
+int solution(string numbers) {
+    int answer = 0;
+    return answer;
+}
+
+void main() {
+}
